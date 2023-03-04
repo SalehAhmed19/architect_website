c7d58e086a78361660da52a0096ab6157b03d744
salehAhmed19 <saleh.ahmed.mahin@gmail.com>
2023-03-04 18:29:25 +0600

    scroll to top

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import DrawerAppBar from "./Shared/DrawerAppBar";
 import Home from "./Pages/Home";
 import Footer from "./Shared/Footer";
+import ScrollToTop from "react-scroll-to-top";
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 
 function App() {
   return (
@@ -9,6 +11,23 @@ function App() {
       <DrawerAppBar />
       <Home />
       <Footer />
+      <ScrollToTop
+        color="#02411C"
+        style={{
+          boxShadow: "none",
+          backgroundColor: "#D0011C",
+          borderRadius: "50%",
+          right: 50,
+          zIndex: 20,
+        }}
+        component={
+          <MdOutlineKeyboardDoubleArrowUp
+            style={{ fontSize: "20px", margin: "0 auto", color: "#fff" }}
+          />
+        }
+        smooth
+        top={500}
+      />
     </div>
   );
 }
